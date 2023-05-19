4443efa04316d09dab7c9f2d83867eae8d038043
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-05-19 11:15:37 -0300

    fix: lens and token services  tests

--- apps/frontend/src/app/services/lens/lens.service.spec.ts
+++ apps/frontend/src/app/services/lens/lens.service.spec.ts
@@ -1,13 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LensService } from './lens.service';
+import { EthersService } from '../ethers';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
 describe('LensService', () => {
   let service: LensService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: EthersService,
+          useValue: { ethersProvider: { getSigner: jest.fn() } },
+        },
+      ],
+    });
     service = TestBed.inject(LensService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
--- apps/frontend/src/app/services/token/token.service.spec.ts
+++ apps/frontend/src/app/services/token/token.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TokenService } from './token.service';
+import { LensService } from '../lens';
 
 describe('TokenService', () => {
   let service: TokenService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: LensService, useValue: { ethersService: {} } }],
+    });
     service = TestBed.inject(TokenService);
   });
 
