8fbbeb3274d152a26ff00ca466bec8c78f865be6
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-05-19 09:22:41 -0300

    test: example test

--- apps/frontend/src/app/services/squad/squad.service.spec.ts
+++ apps/frontend/src/app/services/squad/squad.service.spec.ts
@@ -1,12 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 import { SquadService } from './squad.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TokenService } from '../token';
 
 describe('SquadService', () => {
   let service: SquadService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: TokenService,
+          useValue: {
+            lensService: { ethersService: {} },
+          },
+        },
+      ],
+    });
+
     service = TestBed.inject(SquadService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
