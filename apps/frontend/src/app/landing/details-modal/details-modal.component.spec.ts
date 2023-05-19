60a898eca10cbadc20aac762cbca856f187a8e8f
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-19 13:50:45 -0300

    test: add details modal test

--- apps/frontend/src/app/landing/details-modal/details-modal.component.spec.ts
+++ apps/frontend/src/app/landing/details-modal/details-modal.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DetailsModalComponent } from './details-modal.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BaseModalModule } from '../../shared/base-modal/base-modal.module';
+import { AboutComponent } from './about';
+import { AreasOfExpertiseComponent } from './areas-of-expertise';
+import { AreasOfInterestComponent } from './areas-of-interest';
+import { AvatarsComponent } from './avatars';
+import { EducationComponent } from './education';
+import { PersonalInfoComponent } from './personal-info';
+import { ProfessionalExperienceComponent } from './professional-experience';
+import { RecommendationsComponent } from './recommendations';
+import { SkillsAndCompetenciesComponent } from './skills-and-competencies';
+import { SquadBannerComponent } from './squad-banner';
 
 describe('DetailsModalComponent', () => {
   let component: DetailsModalComponent;
@@ -8,16 +19,26 @@ describe('DetailsModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsModalComponent ]
-    })
-    .compileComponents();
-
+      declarations: [
+        DetailsModalComponent,
+        AvatarsComponent,
+        PersonalInfoComponent,
+        AboutComponent,
+        EducationComponent,
+        ProfessionalExperienceComponent,
+        AreasOfExpertiseComponent,
+        AreasOfInterestComponent,
+        SkillsAndCompetenciesComponent,
+        SquadBannerComponent,
+        RecommendationsComponent,
+      ],
+      imports: [BaseModalModule, RouterTestingModule],
+    }).compileComponents();
     fixture = TestBed.createComponent(DetailsModalComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component details modal', () => {
     expect(component).toBeTruthy();
   });
 });
