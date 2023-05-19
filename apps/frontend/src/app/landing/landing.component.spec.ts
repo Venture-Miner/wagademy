7e1daf8c4ddd642053f044eac49c6442af27c953
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-19 11:33:07 -0300

    test: add landing tests

--- apps/frontend/src/app/landing/landing.component.spec.ts
+++ apps/frontend/src/app/landing/landing.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LandingComponent } from './landing.component';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ButtonPrimaryModule } from '../shared/button-primary/button-primary.module';
+import { ButtonSecondaryModule } from '../shared/button-secondary/button-secondary.module';
+import { CardComponent } from './card';
+import { FooterComponent } from './footer';
+import { NavbarLandingComponent } from './navbar-landing';
+import { ResumesTogglerComponent } from './resumes-toggler';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,16 +15,52 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LandingComponent,
+        CardComponent,
+        NavbarLandingComponent,
+        FooterComponent,
+        ResumesTogglerComponent,
+      ],
+      imports: [
+        ButtonSecondaryModule,
+        RouterTestingModule,
+        ButtonPrimaryModule,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component landing', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to "account type"', () => {
+    const routerLink = fixture.debugElement
+      .query(By.css('#account-type'))
+      .nativeElement.getAttribute('ng-reflect-router-link');
+    expect(routerLink).toBe('/account-type');
+  });
+
+  it('should display a list of student', () => {
+    component.selectedBlock === 'STUDENTS';
+    fixture.detectChanges();
+    const studentList = fixture.debugElement.queryAll(By.css('#student-list'));
+    expect(studentList.length).toBe(4);
+  });
+
+  it('should display a list of team', () => {
+    component.selectedBlock === 'SQUADS';
+    fixture.detectChanges();
+    const teamList = fixture.debugElement.queryAll(By.css('#team-list'));
+    expect(teamList.length).toBe(0);
+  });
+
+  it('should display a list of teacher', () => {
+    const teacherList = fixture.debugElement.queryAll(By.css('#teacher-list'));
+    expect(teacherList.length).toBe(1);
   });
 });
