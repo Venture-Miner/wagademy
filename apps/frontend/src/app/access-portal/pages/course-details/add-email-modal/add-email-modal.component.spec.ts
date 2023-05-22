8fea7990e191b417f54a0428e6aa8e0d19605b3f
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-22 12:57:48 -0300

    test: add email modal component test

--- apps/frontend/src/app/access-portal/pages/course-details/add-email-modal/add-email-modal.component.spec.ts
+++ apps/frontend/src/app/access-portal/pages/course-details/add-email-modal/add-email-modal.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AddEmailModalComponent } from './add-email-modal.component';
+import { ButtonSecondaryModule } from 'apps/frontend/src/app/shared/button-secondary/button-secondary.module';
+import { ButtonPrimaryModule } from 'apps/frontend/src/app/shared/button-primary/button-primary.module';
+import { By } from '@angular/platform-browser';
 
 describe('AddEmailModalComponent', () => {
   let component: AddEmailModalComponent;
@@ -8,6 +11,7 @@ describe('AddEmailModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddEmailModalComponent],
+      imports: [ButtonSecondaryModule, ButtonPrimaryModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddEmailModalComponent);
@@ -15,7 +19,16 @@ describe('AddEmailModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component add email modal', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call emailConfirm()', () => {
+    const emailConfirm = jest.spyOn(component, 'emailConfirm');
+    const emailConfirmButton = fixture.debugElement.query(
+      By.css('#email-confirm')
+    );
+    emailConfirmButton.nativeElement.click();
+    expect(emailConfirm).toHaveBeenCalledTimes(1);
   });
 });
