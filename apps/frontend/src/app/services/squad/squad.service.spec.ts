cb53018ef1d044d687309842fe843b32eda3252b
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-05-22 15:17:37 -0300

    test: squad service test

--- apps/frontend/src/app/services/squad/squad.service.spec.ts
+++ apps/frontend/src/app/services/squad/squad.service.spec.ts
@@ -5,6 +5,14 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TokenService } from '../token';
+import { Squad } from '../../interfaces';
+
+const squad: Squad = {
+  id: 'any_id',
+  name: 'any_name',
+  members: ['any_member'],
+  owner: 'any_owner',
+};
 
 describe('SquadService', () => {
   let service: SquadService;
@@ -18,6 +26,7 @@ describe('SquadService', () => {
           provide: TokenService,
           useValue: {
             lensService: { ethersService: {} },
+            getTokenValue: jest.fn().mockReturnValue('any_valid_token'),
           },
         },
       ],
@@ -33,5 +42,73 @@ describe('SquadService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get squads', (done) => {
+    service.getSquads().subscribe((response) => {
+      expect(response).toEqual([squad]);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/squad`);
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush([squad]);
+  });
+
+  it('should get squad', (done) => {
+    const id = squad.id;
+    service.getSquad(id).subscribe((response) => {
+      expect(response).toEqual(squad);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/squad/${id}`);
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(squad);
+  });
+
+  it('should create a squad', (done) => {
+    service.createSquad({ name: 'any_name' }).subscribe((response) => {
+      expect(response).toEqual(squad);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/squad`);
+
+    expect(request.request.method).toBe('POST');
+
+    request.flush(squad);
+  });
+
+  it('should join a squad', (done) => {
+    const id = squad.id;
+    service.joinSquad(id).subscribe((response) => {
+      expect(response).toEqual(squad);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/squad/join/${id}`);
+
+    expect(request.request.method).toBe('PATCH');
+
+    request.flush(squad);
+  });
+
+  it('should quit a squad', (done) => {
+    const id = squad.id;
+    service.quitSquad(id).subscribe((response) => {
+      expect(response).toEqual(squad);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/squad/quit/${id}`);
+
+    expect(request.request.method).toBe('PATCH');
+
+    request.flush(squad);
   });
 });
