d9a77d892c86f713bd7a544a63caf907383cfdfd
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-22 13:02:31 -0300

    test: email confirm modal component tests

--- apps/frontend/src/app/access-portal/pages/course-details/email-confirm-modal/email-confirm-modal.component.spec.ts
+++ apps/frontend/src/app/access-portal/pages/course-details/email-confirm-modal/email-confirm-modal.component.spec.ts
@@ -1,5 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EmailConfirmModalComponent } from './email-confirm-modal.component';
+import { SuccessModalModule } from 'apps/frontend/src/app/shared/success-modal/success-modal.module';
+import { By } from '@angular/platform-browser';
+import { InputModule } from 'apps/frontend/src/app/shared/input/input.module';
+import { ButtonSecondaryModule } from 'apps/frontend/src/app/shared/button-secondary/button-secondary.module';
+import { ButtonPrimaryModule } from 'apps/frontend/src/app/shared/button-primary/button-primary.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('EmailConfirmModalComponent', () => {
   let component: EmailConfirmModalComponent;
@@ -8,6 +14,13 @@ describe('EmailConfirmModalComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EmailConfirmModalComponent],
+      imports: [
+        SuccessModalModule,
+        InputModule,
+        ButtonSecondaryModule,
+        ButtonPrimaryModule,
+        ReactiveFormsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmailConfirmModalComponent);
@@ -15,7 +28,16 @@ describe('EmailConfirmModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component email confirm modal', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call confirmEmail()', () => {
+    const confirmEmail = jest.spyOn(component, 'confirmEmail');
+    const confirmEmailButton = fixture.debugElement.query(
+      By.css('#confirm-email')
+    );
+    confirmEmailButton.nativeElement.click();
+    expect(confirmEmail).toHaveBeenCalledTimes(1);
   });
 });
