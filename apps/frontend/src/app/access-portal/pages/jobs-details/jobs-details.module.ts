f2fc61b71e36926f57bd03e226cf5b0faaf28718
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-22 09:51:32 -0300

    feat: add jobs details component

---
+++ apps/frontend/src/app/access-portal/pages/jobs-details/jobs-details.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobsDetailsComponent } from './jobs-details.component';
+import { JobsDetailsRoutingModule } from './jobs-details-routing.module';
+import { JobCardDetailsComponent } from './components/job-card-details';
+import { ButtonPrimaryModule } from '../../../shared/button-primary/button-primary.module';
+import { ButtonSecondaryModule } from '../../../shared/button-secondary/button-secondary.module';
+import { InputModule } from '../../../shared/input/input.module';
+import { NavbarAuthenticatedModule } from '../../../shared/navbar-authenticated/navbar-authenticated.module';
+
+@NgModule({
+  declarations: [JobsDetailsComponent, JobCardDetailsComponent],
+  imports: [
+    CommonModule,
+    JobsDetailsRoutingModule,
+    ButtonPrimaryModule,
+    ButtonSecondaryModule,
+    NavbarAuthenticatedModule,
+    InputModule,
+  ],
+})
+export class JobsDetailsModule {}
