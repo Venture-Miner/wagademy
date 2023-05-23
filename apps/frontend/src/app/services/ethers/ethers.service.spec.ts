914ecae2f66613501fc34f99b7a1ebfba5fc3913
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-05-23 12:16:48 -0300

    test: ensurer signedTypeData is being called

--- apps/frontend/src/app/services/ethers/ethers.service.spec.ts
+++ apps/frontend/src/app/services/ethers/ethers.service.spec.ts
@@ -1,7 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { EthersService } from './ethers.service';
 
-jest.mock('ethers');
+jest.mock('ethers', () => {
+  return {
+    ethers: {
+      providers: {
+        Web3Provider: jest.fn(() => ({
+          getSigner: jest.fn(() => ({
+            _signTypedData: jest.fn(),
+          })),
+        })),
+      },
+    },
+  };
+});
 
 describe('EthersService', () => {
   let service: EthersService;
@@ -13,5 +25,20 @@ describe('EthersService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call signedTypeData', async () => {
+    const signedTypeDataSpyOn = jest.spyOn(service, 'signedTypeData');
+    await service.signedTypeData(
+      {
+        name: 'any_name',
+        chainId: 'any_id',
+        salt: 'any_salt',
+        version: 'any_version',
+      },
+      {},
+      {}
+    );
+    expect(signedTypeDataSpyOn).toHaveBeenCalledTimes(1);
   });
 });
