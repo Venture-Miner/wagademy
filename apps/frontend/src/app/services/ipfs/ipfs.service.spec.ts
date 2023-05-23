f91430079827669e83bcb7e1ad25847770bc4b85
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-05-23 14:45:41 -0300

    test: ensures calls correctly backend for ipfs save

--- apps/frontend/src/app/services/ipfs/ipfs.service.spec.ts
+++ apps/frontend/src/app/services/ipfs/ipfs.service.spec.ts
@@ -1,20 +1,53 @@
 import { TestBed } from '@angular/core/testing';
 import { IpfsService } from './ipfs.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { v4 as uuidv4 } from 'uuid';
 
 jest.mock('ethers');
-
+const ipfsBody = {
+  version: '2.0.0',
+  mainContentFocus: 'TEXT_ONLY',
+  description: 'Academy Curriculum',
+  metadata_id: uuidv4(),
+  locale: 'en-US',
+  content: JSON.stringify({ curriculum: 'curriculum' }),
+  external_url: null,
+  image: null,
+  imageMimeType: null,
+  name: `@any_handle Curriculum`,
+  attributes: [],
+  tags: [],
+  appId: 'Academy',
+};
 describe('IpfsService', () => {
   let service: IpfsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(IpfsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save a post in ipfs', (done) => {
+    service.uploadIpfs(ipfsBody).subscribe((response) => {
+      expect(response).toEqual(ipfsBody);
+      done();
+    });
+
+    const request = httpMock.expectOne(`${service.URL}/ipfs/create-post`);
+
+    expect(request.request.method).toBe('POST');
+
+    request.flush(ipfsBody);
   });
 });
