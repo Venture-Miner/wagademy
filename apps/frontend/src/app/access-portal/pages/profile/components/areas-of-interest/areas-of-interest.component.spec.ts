47f7915e5af15d03270e000a5b6be46e02520738
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-24 10:11:27 -0300

    test: areas of interest component tests

--- apps/frontend/src/app/access-portal/pages/profile/components/areas-of-interest/areas-of-interest.component.spec.ts
+++ apps/frontend/src/app/access-portal/pages/profile/components/areas-of-interest/areas-of-interest.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AreasOfInterestComponent } from './areas-of-interest.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AreasOfInterestComponent', () => {
   let component: AreasOfInterestComponent;
@@ -8,12 +10,21 @@ describe('AreasOfInterestComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AreasOfInterestComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
     fixture = TestBed.createComponent(AreasOfInterestComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component areas of interest', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to "profile edit"', () => {
+    const routerLink = fixture.debugElement
+      .query(By.css('#redirect-profile-edit'))
+      .nativeElement.getAttribute('ng-reflect-router-link');
+    expect(routerLink).toBe('/home/profile/profile-edit');
   });
 });
