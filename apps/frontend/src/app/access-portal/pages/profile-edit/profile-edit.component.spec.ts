aede193eeee4fca3c12f015633c71b622c43a543
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-25 11:27:33 -0300

    test: profile edit component tests

--- apps/frontend/src/app/access-portal/pages/profile-edit/profile-edit.component.spec.ts
+++ apps/frontend/src/app/access-portal/pages/profile-edit/profile-edit.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileEditComponent } from './profile-edit.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarAuthenticatedModule } from '../../../shared/navbar-authenticated/navbar-authenticated.module';
+import { By } from '@angular/platform-browser';
 
 jest.mock('ethers');
 
@@ -16,9 +17,17 @@ describe('ProfileEditComponent', () => {
     }).compileComponents();
     fixture = TestBed.createComponent(ProfileEditComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component profile edit', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to "home"', () => {
+    const routerLink = fixture.debugElement
+      .query(By.css('#redirect-home'))
+      .nativeElement.getAttribute('ng-reflect-router-link');
+    expect(routerLink).toBe('/home/profile');
   });
 });
