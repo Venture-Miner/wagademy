6491aa209bf9a9b3be4e94ff8dd3993e3bffba13
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-26 11:11:41 -0300

    feat: company card component

---
+++ apps/frontend/src/app/access-portal/pages/home-company/components/company-card/company-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lens-academy-company-card',
+  templateUrl: './company-card.component.html',
+  styleUrls: ['./company-card.component.css'],
+})
+export class CompanyCardComponent {}
