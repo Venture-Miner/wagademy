cd3826c60c51c6e4680a05c3e490ee6bbb66943b
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-30 09:59:14 -0300

    feat: add form at details components

--- apps/frontend/src/app/access-portal/pages/home-company/components/details-modal/details-modal.component.ts
+++ apps/frontend/src/app/access-portal/pages/home-company/components/details-modal/details-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'lens-academy-details-modal',
@@ -8,4 +9,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class DetailsModalComponent {
   @Output() cancel = new EventEmitter<void>();
   showDetailsModal = false;
+  form = this.fb.group({
+    course: ['', [Validators.required]],
+    institution: ['', [Validators.required]],
+    hours: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
 }
