b10708832245c0b0aab0e3ddf937c2654c01e525
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-31 10:18:59 -0300

    refactor: course card component

--- apps/frontend/src/app/access-portal/pages/home/components/course-card/course-card.component.ts
+++ apps/frontend/src/app/access-portal/pages/home/components/course-card/course-card.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lens-academy-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css'],
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() image = '';
+}
