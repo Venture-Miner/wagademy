ece91f89ab63045c2ab9c3ad9415ac96418b8b8e
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2023-05-31 10:18:20 -0300

    feat: add post card component

---
+++ apps/frontend/src/app/access-portal/pages/home/components/post-card/post-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lens-academy-post-card',
+  templateUrl: './post-card.component.html',
+  styleUrls: ['./post-card.component.css'],
+})
+export class PostCardComponent {}
