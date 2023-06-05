33ce4594ee546fd61a06d729b18ed682415b2e4d
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-06-05 14:23:43 -0300

    fix: added guards that was lacking

--- apps/frontend/src/app/access-portal/pages/hired/hired-routing.module.ts
+++ apps/frontend/src/app/access-portal/pages/hired/hired-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HiredComponent } from './hired.component';
+import { AccountTypeCompanyGuard } from '../../../guards/account-type-company.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: HiredComponent,
+    canActivate: [AccountTypeCompanyGuard],
   },
 ];
 
--- apps/frontend/src/app/access-portal/pages/home-company/home-company-routing.module.ts
+++ apps/frontend/src/app/access-portal/pages/home-company/home-company-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeCompanyComponent } from './home-company.component';
+import { AccountTypeCompanyGuard } from '../../../guards/account-type-company.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeCompanyComponent,
+    canActivate: [AccountTypeCompanyGuard],
   },
 ];
 
