a04b83f8ae6b06e245e2177975b1f46c8ee520f5
Eduardo Broetto <eduardo_abg@hotmail.com>
2023-06-05 14:02:54 -0300

    feat: squad throw an error when name is not unique

--- apps/api/src/modules/squad/squad.service.ts
+++ apps/api/src/modules/squad/squad.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@prisma-service';
 import { Prisma } from '@prisma/client';
 
@@ -7,7 +7,19 @@ export class SquadService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(data: Prisma.SquadCreateInput) {
-    return this.prismaService.squad.create({ data });
+    await this.prismaService.squad
+      .create({ data })
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        if (error.meta.target === 'Squad_name_key')
+          throw new BadRequestException(
+            { ...error },
+            'Squad name already exists'
+          );
+        else throw new BadRequestException({ ...error });
+      });
   }
 
   findAll() {
