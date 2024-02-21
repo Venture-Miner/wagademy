30490a1d385f330955875248a66f212ea93f7dca
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-21 20:15:36 -0300

    refactor: landing component

--- apps/wagademy-mvp-2/src/app/landing/landing.component.ts
+++ apps/wagademy-mvp-2/src/app/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'wagademy-landing',
   templateUrl: './landing.component.html',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent, FooterComponent],
 })
 export class LandingComponent {}
