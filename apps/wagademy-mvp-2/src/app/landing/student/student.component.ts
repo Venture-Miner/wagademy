935b5149f6a7b5cba3ee263a35fd504f9bdba658
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-21 22:27:48 -0300

    feat: student component

--- apps/wagademy-mvp-2/src/app/landing/student/student.component.ts
+++ apps/wagademy-mvp-2/src/app/landing/student/student.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'wagademy-student',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss',
 })
