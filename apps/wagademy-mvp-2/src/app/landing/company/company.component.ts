0e2476b21b19a2e93c694a5dad3a810daf93f8f8
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-22 08:30:00 -0300

    feat: company component

--- apps/wagademy-mvp-2/src/app/landing/company/company.component.ts
+++ apps/wagademy-mvp-2/src/app/landing/company/company.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'wagademy-company',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './company.component.html',
   styleUrl: './company.component.scss',
 })
