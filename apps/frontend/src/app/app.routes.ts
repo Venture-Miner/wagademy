a538b63a1f50efbc2f4d294be0a1f4d8c8073a3e
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-29 12:24:39 -0300

    refactor: app routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -6,4 +6,9 @@ export const appRoutes: Routes = [
     loadChildren: () =>
       import('./landing/landing.module').then((m) => m.LandingModule),
   },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./account/account.module').then((m) => m.AccountModule),
+  },
 ];
