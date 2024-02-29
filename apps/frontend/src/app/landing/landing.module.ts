0798a2173b7aaf81ff32e5fa23f60c89a742a2ad
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-29 20:29:39 -0300

    refactor: lazyload components

--- apps/frontend/src/app/landing/landing.module.ts
+++ apps/frontend/src/app/landing/landing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarModule } from './layout/navbar/navbar.module';
+import { FooterModule } from './layout/footer/footer.module';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [
-    CommonModule,
-    LandingRoutingModule,
-    NavbarComponent,
-    FooterComponent,
-  ],
+  imports: [CommonModule, LandingRoutingModule, NavbarModule, FooterModule],
   providers: [],
 })
 export class LandingModule {}
