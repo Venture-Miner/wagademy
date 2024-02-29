e610799732683f1cb13ab595a136588bb79693bb
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-02-29 20:07:12 -0300

    fix: landing routing

--- apps/frontend/src/app/landing/landing-routing.module.ts
+++ apps/frontend/src/app/landing/landing-routing.module.ts
@@ -5,7 +5,7 @@ import { LandingComponent } from './landing.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'Student',
+    redirectTo: 'student',
     pathMatch: 'full',
   },
   {
