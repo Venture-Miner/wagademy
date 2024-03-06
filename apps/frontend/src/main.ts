e01612df423ee7e1e5c4e71ff170b4e4ccff4ef5
Bianca <bruninhakjurugat11@gmail.com>
2024-03-06 17:25:06 -0300

    chore: configure amplify

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Amplify } from 'aws-amplify';
+import { environment } from './environments/environment.development';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
@@ -11,3 +13,12 @@ declare global {
     filesUpload: HTMLFormElement;
   }
 }
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: environment.userPoolId,
+      userPoolClientId: environment.userPoolClientId,
+    },
+  },
+});
