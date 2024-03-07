73e1bd86d8ff08d236bf662c2977fc8600b17e57
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-03-07 09:18:13 -0300

    refactor: pages component

---
+++ apps/frontend/src/app/pages/pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'wagademy-pages',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './pages.component.html',
+})
+export class PagesComponent {}
