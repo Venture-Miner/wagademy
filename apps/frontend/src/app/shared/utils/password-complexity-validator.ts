774cc34e53e188d2882e9271c1fe242470c71cce
Bianca <bruninhakjurugat11@gmail.com>
2024-03-08 12:00:41 -0300

    fix: password policy

--- apps/frontend/src/app/shared/utils/password-complexity-validator.ts
+++ apps/frontend/src/app/shared/utils/password-complexity-validator.ts
@@ -13,27 +13,27 @@ export const passwordComplexityValidator: ValidatorFn = (
     return null;
   }
 
-  const hasUpperAndLowerCases = /[a-z]+/.test(value) && /[A-Z]+/.test(value);
-  const hasNumbersAndLetters = /[a-zA-Z]+/.test(value) && /[0-9]+/.test(value);
-  const hasSpecialCharacters = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(
-    value
-  );
+  // const hasUpperAndLowerCases = /[a-z]+/.test(value) && /[A-Z]+/.test(value);
+  // const hasNumbersAndLetters = /[a-zA-Z]+/.test(value) && /[0-9]+/.test(value);
+  // const hasSpecialCharacters = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(
+  //   value
+  // );
   const hasMinimumLength = value.length >= 8;
 
   const passwordValid =
-    hasUpperAndLowerCases &&
-    hasNumbersAndLetters &&
-    hasSpecialCharacters &&
+    // hasUpperAndLowerCases &&
+    // hasNumbersAndLetters &&
+    // hasSpecialCharacters &&
     hasMinimumLength;
 
   const errors: PasswordComplexityErrors = [
     { message: 'At least 8 characters', isValid: hasMinimumLength },
-    { message: 'Numbers and letters', isValid: hasNumbersAndLetters },
-    {
-      message: 'Upper and lower case letters',
-      isValid: hasUpperAndLowerCases,
-    },
-    { message: 'One special character', isValid: hasSpecialCharacters },
+    // { message: 'Numbers and letters', isValid: hasNumbersAndLetters },
+    // {
+    //   message: 'Upper and lower case letters',
+    //   isValid: hasUpperAndLowerCases,
+    // },
+    // { message: 'One special character', isValid: hasSpecialCharacters },
   ];
 
   return !passwordValid ? { customvalidator: errors } : null;
