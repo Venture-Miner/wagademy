28171b9285ab6686c4ac045280348311976163e1
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-03-11 13:48:01 -0300

    feat: start development courses component

---
+++ apps/frontend/src/app/pages/courses/courses/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wagademy-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss',
+})
+export class CoursesComponent {}
