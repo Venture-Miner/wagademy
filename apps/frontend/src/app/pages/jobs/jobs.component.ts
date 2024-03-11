958e1ea0b0c1822036324cb89389a092aec9b628
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-03-11 19:12:36 -0300

    feat: start development jobs component

---
+++ apps/frontend/src/app/pages/jobs/jobs.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { CardsComponent } from '../../shared/components/cards/cards.component';
+import { InputComponent } from '../../shared/components/input/input.component';
+
+interface Filter {
+  name: string;
+}
+
+@Component({
+  selector: 'wagademy-jobs',
+  standalone: true,
+  imports: [InputComponent, NgIf, NgFor, CardsComponent],
+  templateUrl: './jobs.component.html',
+  styleUrl: './jobs.component.scss',
+})
+export class JobsComponent {
+  jobs = [
+    {
+      images: '',
+      name: 'Job name',
+      description:
+        'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu. Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu',
+    },
+    {
+      images: '',
+      name: 'Job name',
+      description:
+        'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu. Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu',
+    },
+    {
+      images: '',
+      name: 'Job name',
+      description:
+        'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu. Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu',
+    },
+    {
+      images: '',
+      name: 'Job name',
+      description:
+        'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu. Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam, ultricies sollicitudin erat facilisis eget. Vestibulum rhoncus dui vel eros laoreet consectetur. Vivamus eget elementum ligula, vitae pharetra quam. Nullam at ligula sed metu',
+    },
+  ];
+  isLoading = false;
+  filters: Filter[] = [
+    { name: 'All' },
+    { name: 'Featured' },
+    { name:  'Most recent' },
+    { name: 'Most accessed' },
+  ]
+}
