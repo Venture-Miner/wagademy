7761306f972cec3d86b1e783cf58771becfa6785
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-03-12 15:35:02 -0300

    refactor: start of card refactoring for the home company

--- apps/frontend/src/app/shared/components/cards/cards.component.ts
+++ apps/frontend/src/app/shared/components/cards/cards.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,6 +16,18 @@ export class CardsComponent {
   @Input() selectId: string | undefined;
   @Input() button: string | undefined;
   @Input() applications: number | undefined;
+  @Input() courses = false;
+  @Input() jobs = false;
+  showMenu = false;
+  showMenuCourse = false;
+  @Output() unpublishJob: EventEmitter<void> = new EventEmitter<void>();
+  @Output() updateJob: EventEmitter<void> = new EventEmitter<void>();
+  @Output() removeJob: EventEmitter<void> = new EventEmitter<void>();
+  @Output() interviewGPTJob: EventEmitter<void> = new EventEmitter<void>();
+  @Output() copyLinkJob: EventEmitter<void> = new EventEmitter<void>();
+  @Output() updateCourse: EventEmitter<void> = new EventEmitter<void>();
+  @Output() removeCourse: EventEmitter<void> = new EventEmitter<void>();
+  @Output() certificatesCourse: EventEmitter<void> = new EventEmitter<void>();
 
   constructor(private router: Router) {}
 
