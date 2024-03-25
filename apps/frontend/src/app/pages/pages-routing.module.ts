a1712f6e3390227b7b2430956d4f9e421ae30925
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-03-25 10:17:09 -0300

    refactor: pages routing

--- apps/frontend/src/app/pages/pages-routing.module.ts
+++ apps/frontend/src/app/pages/pages-routing.module.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import { HomeComponent } from './person/home/home.component';
 import { MyCertificatesComponent } from './certificates/my-certificates.component';
 import { GptChatComponent } from './gpt-chat/gpt-chat.component';
+import { HiringComponent } from './company/hiring/hiring.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,10 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent,
+      },
+      {
+        path: 'hiring',
+        component: HiringComponent,
       },
     ],
   },
