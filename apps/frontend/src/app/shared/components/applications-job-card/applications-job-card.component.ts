4f836a2d86be0c235efe7a1394d0d72ddc0039f6
eduardo <eduardo_abg@hotmail.com>
2024-04-15 09:52:58 -0300

    chore: change conditions from ngif to if

--- apps/frontend/src/app/shared/components/applications-job-card/applications-job-card.component.ts
+++ apps/frontend/src/app/shared/components/applications-job-card/applications-job-card.component.ts
@@ -1,10 +1,11 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { JobApplicationStatusEnum } from '@wagademy/types';
 
 @Component({
   selector: 'wagademy-applications-job-card',
   standalone: true,
-  imports: [NgIf, NgClass],
+  imports: [NgClass],
   templateUrl: './applications-job-card.component.html',
   styleUrls: ['./applications-job-card.component.scss'],
 })
@@ -12,5 +13,5 @@ export class ApplicationsJobCardComponent {
   @Input() name: string | undefined;
   @Input() img: string | undefined;
   @Input() description: string | undefined;
-  @Input() status: string | undefined;
+  @Input() status: JobApplicationStatusEnum | undefined;
 }
