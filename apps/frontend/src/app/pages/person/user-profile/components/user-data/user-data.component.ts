a4805a236e74262f6ff6108bd50305eb0f92a73d
gabrielmsaraiva <gabrielmsaraiva@outlook.com>
2024-04-18 17:41:04 -0300

    feat: user data component

---
+++ apps/frontend/src/app/pages/person/user-profile/components/user-data/user-data.component.ts
@@ -0,0 +1,53 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormFieldComponent } from 'apps/frontend/src/app/shared/components/form-field/form-field.component';
+import { InputComponent } from 'apps/frontend/src/app/shared/components/input/input.component';
+import {
+  SelectComponent,
+  SelectItem,
+} from 'apps/frontend/src/app/shared/components/select/select.component';
+import { TextAreaComponent } from 'apps/frontend/src/app/shared/components/text-area/text-area.component';
+import { PhotoUploadComponent } from '../photo-upload/photo-upload.component';
+
+export type UserData = {
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  birth: FormControl<string | null>;
+  cellphone: FormControl<string | null>;
+  country: FormControl<SelectItem<string> | null>;
+  state: FormControl<SelectItem<string> | null>;
+  about: FormControl<string | null>;
+};
+
+@Component({
+  selector: 'wagademy-user-data',
+  standalone: true,
+  templateUrl: './user-data.component.html',
+  styleUrls: ['./user-data.component.css'],
+  imports: [
+    ReactiveFormsModule,
+    FormFieldComponent,
+    SelectComponent,
+    NgIf,
+    InputComponent,
+    TextAreaComponent,
+    PhotoUploadComponent,
+  ],
+})
+export class UserDataComponent {
+  @Input() userData!: FormGroup<UserData>;
+  @Output() nextStep = new EventEmitter<void>();
+  @Output() imageUploaded = new EventEmitter<File>();
+  imageData: File | undefined;
+
+  mockCountries: SelectItem<string>[] = [
+    { value: 'BR', label: 'Brazil' },
+    { value: 'EUA', label: 'United States' },
+  ];
+
+  mockStates: SelectItem<string>[] = [
+    { value: 'MG', label: 'Minas Gerais' },
+    { value: 'TX', label: 'Texas' },
+  ];
+}
