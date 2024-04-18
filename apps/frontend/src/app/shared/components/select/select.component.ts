7061a7ee8623cf00b6c1fb0c8228c13aefb7478f
gabrielmsaraiva <gabrielmsaraiva@outlook.com>
2024-04-18 17:40:43 -0300

    fix: select component

--- apps/frontend/src/app/shared/components/select/select.component.ts
+++ apps/frontend/src/app/shared/components/select/select.component.ts
@@ -1,5 +1,14 @@
-import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Input,
+  Optional,
+  Renderer2,
+  Self,
+  ViewChild,
+} from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 export interface SelectItem<T> {
   value: T;
@@ -8,12 +17,59 @@ export interface SelectItem<T> {
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgClass],
   selector: 'wagademy-select',
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss',
 })
-export class SelectComponent {
+export class SelectComponent implements ControlValueAccessor {
   @Input() items: SelectItem<string | number | boolean>[] = [];
-  @Input() placeholder = 'Select';
+  @Input() placeholder = 'Select your option';
+
+  @ViewChild('select', { static: true, read: ElementRef })
+  elementRef!: ElementRef;
+
+  constructor(
+    private renderer2: Renderer2,
+    @Self() @Optional() private control?: NgControl
+  ) {
+    if (this.control) this.control.valueAccessor = this;
+  }
+
+  hasError() {
+    return !!(this.control?.invalid && this.control.touched);
+  }
+
+  private onChange = (_: unknown) => {};
+
+  private onTouched = () => {};
+
+  registerOnChange(fn: (_: unknown) => unknown) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => unknown) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: string) {
+    this.renderer2.setProperty(this.elementRef.nativeElement, 'value', value);
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.renderer2.setProperty(
+      this.elementRef.nativeElement,
+      'disabled',
+      isDisabled
+    );
+  }
+
+  onInputChange() {
+    const value = this.elementRef.nativeElement.value;
+    this.onChange(value);
+  }
+
+  onBlur() {
+    this.onTouched();
+  }
 }
