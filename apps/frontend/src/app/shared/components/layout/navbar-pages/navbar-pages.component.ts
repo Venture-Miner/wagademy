fca0aa902c6444834f70cdf1de83cd4f4eafd5a1
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2024-04-18 16:45:22 -0300

    fix: adjust navbar behavior (close when clicked away)

--- apps/frontend/src/app/shared/components/layout/navbar-pages/navbar-pages.component.ts
+++ apps/frontend/src/app/shared/components/layout/navbar-pages/navbar-pages.component.ts
@@ -1,5 +1,11 @@
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AccountTypeEnum } from '@wagademy/types';
 import { AuthService } from '../../../../services/auth/auth.service';
@@ -16,11 +22,34 @@ export class NavbarPagesComponent implements OnInit {
   existingProfileImage = './assets/img/images/img-user-profile.svg';
   openMenu = false;
   accountType!: AccountTypeEnum;
+  @ViewChild('toggleButton') toggleButton!: ElementRef;
+  @ViewChild('dropdown') dropdown!: ElementRef;
+  @ViewChild('toggleNavbarButton') toggleNavbarButton!: ElementRef;
+  @ViewChild('list') list!: ElementRef;
 
   readonly PHYSICAL_PERSON = AccountTypeEnum.PHYSICAL_PERSON;
   readonly COMPANY = AccountTypeEnum.COMPANY;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      const target = e.target as Node;
+      const isOutsideToggleButton =
+        this.toggleButton && !this.toggleButton.nativeElement.contains(target);
+      const isOutsideDropdown =
+        this.dropdown && !this.dropdown.nativeElement.contains(target);
+      const isOutsideNavbarButton =
+        this.toggleNavbarButton &&
+        !this.toggleNavbarButton.nativeElement.contains(target);
+      const isOutsideList =
+        this.list && !this.list.nativeElement.contains(target);
+      if (isOutsideToggleButton && isOutsideDropdown) {
+        this.openMenu = false;
+      }
+      if (isOutsideNavbarButton && isOutsideList) {
+        this.showMenu = false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.authService
